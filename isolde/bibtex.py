418afab9bcfabfabe19673025693d9c77a083dd6
JGarciaCondado <jorgeschool@gmail.com>
2020-09-15 12:20:31 +0200

    Add bibtex ISOLDE

---
+++ isolde/bibtex.py
@@ -0,0 +1,44 @@
+
+# -*- coding: utf-8 -*-
+# **************************************************************************
+# *
+# * Authors:     David Herreros Calero (dherreros@cnb.csic.es)
+# *
+# * BCU, Centro Nacional de Biotecnologia, CSIC
+# *
+# * This program is free software; you can redistribute it and/or modify
+# * it under the terms of the GNU General Public License as published by
+# * the Free Software Foundation; either version 2 of the License, or
+# * (at your option) any later version.
+# *
+# * This program is distributed in the hope that it will be useful,
+# * but WITHOUT ANY WARRANTY; without even the implied warranty of
+# * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# * GNU General Public License for more details.
+# *
+# * You should have received a copy of the GNU General Public License
+# * along with this program; if not, write to the Free Software
+# * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+# * 02111-1307  USA
+# *
+# *  All comments concerning this program package may be sent to the
+# *  e-mail address 'scipion@cnb.csic.es'
+# *
+# **************************************************************************
+
+"""
+@article{CROLL2018,
+author = "Croll, Tristan Ian",
+title = "{{\it ISOLDE}: a physically realistic environment for model building into low-resolution electron-density maps}",
+journal = "Acta Crystallographica Section D",
+year = "2018",
+volume = "74",
+number = "6",
+pages = "519--530",
+month = "Jun",
+doi = {10.1107/S2059798318002425},
+url = {https://doi.org/10.1107/S2059798318002425},
+abstract = {This paper introduces {\it ISOLDE}, a new software package designed to provide an intuitive environment for high-fidelity interactive remodelling/refinement of macromolecular models into electron-density maps. {\it ISOLDE} combines interactive molecular-dynamics flexible fitting with modern molecular-graphics visualization and established structural biology libraries to provide an immersive interface wherein the model constantly acts to maintain physically realistic conformations as the user interacts with it by directly tugging atoms with a mouse or haptic interface or applying/removing restraints. In addition, common validation tasks are accelerated and visualized in real time. Using the recently described 3.8{\AA} resolution cryo-EM structure of the eukaryotic minichromosome maintenance (MCM) helicase complex as a case study, it is demonstrated how {\it ISOLDE} can be used alongside other modern refinement tools to avoid common pitfalls of low-resolution modelling and improve the quality of the final model. A detailed analysis of changes between the initial and final model provides a somewhat sobering insight into the dangers of relying on a small number of validation metrics to judge the quality of a low-resolution model.},
+keywords = {model building, real-space refinement, molecular dynamics, visualization, ISOLDE},
+}
+"""
