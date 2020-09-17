cb341a382afb05012b3e31c92936fcbc9dc57176
JGarciaCondado <jorgeschool@gmail.com>
2020-09-17 10:57:36 +0200

    Add form of protcol

--- isolde/protocols/protocol_isolde.py
+++ isolde/protocols/protocol_isolde.py
@@ -29,74 +29,39 @@
 Describe your python module here:
 This module will provide the traditional Hello world example
 """
-from pyworkflow.protocol import Protocol, params, Integer
-from pyworkflow.utils import Message
 
+from pyworkflow.protocol.params import (PointerParam,
+                                        BooleanParam)
+from pwem.protocols import EMProtocol
 
-class ProtIsolde(Protocol):
-    """ This protocol will print hello world in the console
-     IMPORTANT: Classes names should be unique, better prefix them"""
-    _label = 'Hello world'
+class ProtIsolde(EMProtocol):
+    """ Protocol to run ISOLDE within Chimera """
+    _label = 'isolde operate'
 
-    # -------------------------- DEFINE param functions ----------------------
-    def _defineParams(self, form):
-        """ Define the input parameters that will be used.
-        Params:
-            form: this is the form to be populated with sections and params.
-        """
-        # You need a params to belong to a section:
-        form.addSection(label=Message.LABEL_INPUT)
-        form.addParam('message', params.StringParam,
-                      default='Hello world!',
-                      label='Message', important=True,
-                      help='What will be printed in the console.')
+    # --------------------------- DEFINE param functions --------------------
+    def _defineParams(self, form, doHelp=True):
+        form.addSection(label='Input')
+        form.addParam('inputVolume', PointerParam, pointerClass="Volume",
+                      label='Input Volume', allowsNull=False,
+                      important=True,
+                      help="Volume to process")
+        form.addParam('pdbFileToBeRefined', PointerParam,
+                      pointerClass="AtomStruct", allowsNull=False,
+                      important=True,
+                      label='Atomic structure',
+                      help="PDBx/mmCIF file that you can save after operating "
+                           "with it.")
+        form.addParam('addH', BooleanParam,
+                      label='Add hydrogens to PDB', allowsNull=True,
+                      default = True,
+                      help="Automatically add hydrogens to PDB")
+        form.addParam('hideHC', BooleanParam,
+                      label='Hide non-polar hydrogens', allowsNull=True,
+                      default=True,
+                      help="Automatically hide non-polar hydrogens of PDB")
+        form.addParam('restrainLigands', BooleanParam,
+                      label='Restrain ligands', allowsNull=True,
+                      default=True,
+                      help="Automatically restrain ligands in simulation to"
+                            " avoid them being sent away flying")
 
-        form.addParam('times', params.IntParam,
-                      default=10,
-                      label='Times', important=True,
-                      help='Times the message will be printed.')
-
-        form.addParam('previousCount', params.IntParam,
-                      default=0,
-                      allowsNull=True,
-                      label='Previous count',
-                      help='Previous count of printed messages',
-                      allowsPointers=True)
-
-    # --------------------------- STEPS functions ------------------------------
-    def _insertAllSteps(self):
-        # Insert processing steps
-        self._insertFunctionStep('greetingsStep')
-        self._insertFunctionStep('createOutputStep')
-
-    def greetingsStep(self):
-        # say what the parameter says!!
-
-        for time in range(0, self.times.get()):
-            print(self.message)
-
-    def createOutputStep(self):
-        # register how many times the message has been printed
-        # Now count will be an accumulated value
-        timesPrinted = Integer(self.times.get() + self.previousCount.get())
-        self._defineOutputs(count=timesPrinted)
-
-    # --------------------------- INFO functions -----------------------------------
-    def _summary(self):
-        """ Summarize what the protocol has done"""
-        summary = []
-
-        if self.isFinished():
-            summary.append("This protocol has printed *%s* %i times." % (self.message, self.times))
-        return summary
-
-    def _methods(self):
-        methods = []
-
-        if self.isFinished():
-            methods.append("%s has been printed in this run %i times." % (self.message, self.times))
-            if self.previousCount.hasPointer():
-                methods.append("Accumulated count from previous runs were %i."
-                               " In total, %s messages has been printed."
-                               % (self.previousCount, self.count))
-        return methods
